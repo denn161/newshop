55eac0a16b416bc2aef7288ee0d56a842758cbab
Denis <denn161inbox.ru>
2021-03-29 23:43:13 +0300

    пытался сделать выдвигающееся меню, но пока не вышло

--- js/main.js
+++ js/main.js
@@ -1,24 +1,67 @@
-const  burger = document.querySelector('.burger'),
-    close = document.querySelector('.nav__close-btn'),
-       menu = document.querySelector('.header__menu');
+
+$(function () {
+  $('.slider').slick({
+    arrows: false,
+    fade: true,
+    autoplay: 3000,
+    dots: true
 
 
 
 
+  });
+
+  $('.burger').on('click', function () {
+    $('.header__menu').addClass('menu--visibel');
 
 
 
-burger.addEventListener('click',  function() {
-    menu.classList.add('header__menu--visebel');
-        
+  });
+  $('.nav__close-btn').on('click', function () {
+    $('.header__menu').removeClass('menu--visibel');
+
+  });
+
 
 
 });
 
-  close.addEventListener('click',  function() {
-    menu.classList.remove('header__menu--visebel');
 
 
 
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const  burger = document.querySelector('.burger'),
+//     close = document.querySelector('.nav__close-btn'),
+//        menu = document.querySelector('.header__menu');
+
+// burger.addEventListener('click',  function() {
+//     menu.classList.add('header__menu--visebel');      
+
+
+// });
+
+//   close.addEventListener('click',  function() {
+//     menu.classList.remove('header__menu--visebel');
+
+
+
+// });
 
