520f7441532497bd8a00ff8fbd0776c72b2e53f9
Denis <denn161inbox.ru>
2021-04-02 23:02:09 +0300

    сделал js для менюхи справа

--- js/main.js
+++ js/main.js
@@ -1,30 +1,77 @@
-
-$(function () {
-  $('.slider').slick({
-    arrows: false,
-    fade: true,
-    autoplay: 3000,
-    dots: true
+const hb = document.querySelector('.header__btn');
+const menu = document.querySelector('.header__menu');
+const close = document.querySelector('.menu__close-btn');
 
 
 
-
-  });
-
-  $('.burger').on('click', function () {
-    $('.header__menu').addClass('menu--visibel');
+hb.addEventListener('click', function (e) {
+  e.preventDefault();
 
 
-
-  });
-  $('.nav__close-btn').on('click', function () {
-    $('.header__menu').removeClass('menu--visibel');
-
-  });
-
+  menu.classList.add('active'); 
 
 
 });
+close.addEventListener('click', function () {
+  menu.classList.remove('active');
+  
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(function () {
+//   $('.slider').slick({
+//     arrows: false,
+//     fade: true,
+//     autoplay: 3000,
+//     dots: true
+
+
+
+
+//   });
+
+//   $('.burger').on('click', function () {
+//     $('.header__menu').addClass('menu--visibel');
+
+
+
+//   });
+//   $('.nav__close-btn').on('click', function () {
+//     $('.header__menu').removeClass('menu--visibel');
+
+//   });
+
+
+
+// });
 
 
 
